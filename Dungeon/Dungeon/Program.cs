c08ffcdcee2bf5b3137f2aaf58b1c21e837544e2
Romeo Bagunu <romeo.bagunu@outlook.com>
2022-11-08 20:21:19 -0600

    Dungeon project with wireframe.

---
+++ Dungeon/Dungeon/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Dungeon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Introduction
+            //Print a welcome.
+            #endregion
+
+            #region Create Player
+            //TODO: Create an instance of the Player class.
+            #endregion
+
+            #region Gameplay Loop
+
+            #region Create Room & Monster
+
+            //TODO: Print a random room description.
+            //TODO: Create an instance of a Monster class at random.
+
+            #endregion
+
+            #region Menu Loop
+
+            /*
+                TODO: Create menu with options:
+                1) Attack
+                2) Run Away
+                3) Character Info
+                4) Monster Info
+                5) Exit
+             */
+
+            #endregion
+
+            #endregion
+
+        }//end Main()
+
+        private static string GetRoom()
+        {
+            //Requirements:
+            /*
+                1. Create a collection of room descriptions.
+                2. Randomly print one of those room descriptions to the Console.
+             */
+        }
+    }//end class()
+}//end namespace
